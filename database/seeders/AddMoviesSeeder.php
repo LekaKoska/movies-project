d7c1153e39f87026da2085ae9365813423511cb6
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-02-12 12:33:54 +0100

    * Making movie seeder

---
+++ database/seeders/AddMoviesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddMoviesSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $faker = Factory::create();
+
+
+    }
+}
