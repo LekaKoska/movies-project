6fbc4df276d4320c8cdb36ab7edc3de4d61f5177
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-02-18 10:17:14 +0100

    * Permalink movies

---
+++ resources/views/movies/moviePermalink.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+@extends("layout")
+
+@section("content")
+   <h2> {{$movie->title}}</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/pQab51QNjAU?si=iO_MEwJmxFVj7ZGx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<form action="{{route("movies.addComment", ["movie" => $movie->id ])}}" method="POST">
+    {{csrf_field()}}
+
+<textarea placeholder="Enter a comment for this movie.." name="comment"></textarea>
+<button>Send</button>
+</form>
+
+@endsection
+</body>
+</html>
