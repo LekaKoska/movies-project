0e02994939db343381a9375a2d7fb928271beece
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-04 10:39:13 +0200

    * Editing allMovies blade

--- resources/views/movies/allMovies.blade.php
+++ resources/views/movies/allMovies.blade.php
@@ -2,32 +2,57 @@
 @extends("layout")
 
 @section("content")
-    @if(Session::has("error"))
-        <p class="text-danger">{{Session::get("error")}}</p>
-        <a class="btn btn-primary" href="/login">Log in</a>
-    @endif
-    @foreach($movies as $movie)
+    <div class="container mt-4">
 
-        <div class="card mt-2 container" style="width: 18rem;">
-
-            <div class="card-body">
-                <h5 class="card-title">{{$movie->title}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{{$movie->genre->genre}}</h6>
-                <p class="card-text">{{$movie->description}}</p>
-                <p class="card-text">{{$movie->author}}</p>
-                @if(in_array($movie->id, $userFavourites))
-                    <a href="{{route("movies.unfavourite", ['movie' => $movie->id])}}" class="card-link">
-                        <i class="fa-solid fa-bookmark"> </i>  </a>
-
-                @else
-                    <a href="{{route("movies.favourite", ['movie' => $movie->id])}}" class="card-link"><i
-                            class="fa-regular fa-bookmark"></i> </a>
-                @endif
-
-                <a href="{{route("movies.permalink", ['movie' => $movie->title])}}" class="card-link">Watch</a>
+        {{-- Error Message --}}
+        @if(Session::has("error"))
+            <div class="alert alert-danger d-flex justify-content-between align-items-center">
+                <span>{{ Session::get("error") }}</span>
+                <a class="btn btn-sm btn-light" href="/login">Log in</a>
             </div>
+        @endif
+
+        {{-- Movies Grid --}}
+        <div class="row g-4">
+            @foreach($movies as $movie)
+                <div class="col-md-4 col-lg-3">
+                    <div class="card h-100 shadow-sm border-0">
+                        {{-- Ako budeš imao slike, možeš ovde staviti <img class="card-img-top" src="..." alt="..."> --}}
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title text-primary">{{ $movie->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $movie->genre->genre }}</h6>
+                            <p class="card-text flex-grow-1">{{ Str::limit($movie->description, 100) }}</p>
+                            <small class="text-muted">By {{ $movie->author }}</small>
+                        </div>
+                        <div class="card-footer bg-white border-0 d-flex justify-content-between">
+                            {{-- Favourite / Unfavourite --}}
+                            @if(in_array($movie->id, $userFavourites))
+                                <a href="{{ route('movies.unfavourite', ['movie' => $movie->id]) }}"
+                                   class="btn btn-sm btn-outline-danger">
+                                    <i class="fa-solid fa-bookmark"></i> Remove
+                                </a>
+                            @else
+                                <a href="{{ route('movies.favourite', ['movie' => $movie->id]) }}"
+                                   class="btn btn-sm btn-outline-primary">
+                                    <i class="fa-regular fa-bookmark"></i> Save
+                                </a>
+                            @endif
+
+                            {{-- Watch button --}}
+                            <a href="{{ route('movies.permalink', ['movie' => $movie->title]) }}"
+                               class="btn btn-sm btn-success">
+                                Watch
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
 
-
-    @endforeach
+        {{-- Pagination --}}
+        <div class="mt-4 d-flex justify-content-center">
+            {{ $movies->links() }}
+        </div>
+    </div>
 @endsection
+
