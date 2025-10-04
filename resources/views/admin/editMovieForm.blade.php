9e9c2b88d7507a82fede691fc51a2b6b17d64f68
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-04 13:18:34 +0200

    * Adding matching param

--- resources/views/admin/editMovieForm.blade.php
+++ resources/views/admin/editMovieForm.blade.php
@@ -9,13 +9,14 @@
     @extends("layout")
 
 @section("content")
-    <form action="{{route("movie.edit", ["saveMovie" => $movie->id ])}}" method="POST">
-        {{csrf_field()}}
-        <input value="{{$movie->title}}" type="text" placeholder="Enter a tittle of movie" name="title">
-        <textarea type="text" placeholder="Enter a description" name="description">{{$movie->description}}</textarea>
+    <form action="{{ route('movie.save', ['movie' => $movie->id]) }}" method="POST">
+        @csrf
+        <input value="{{$movie->title}}" type="text" placeholder="Enter a title" name="title">
+        <textarea placeholder="Enter a description" name="description">{{$movie->description}}</textarea>
         <input value="{{$movie->author}}" type="text" placeholder="Enter author" name="author">
-        <button>Edit</button>
+        <button type="submit">Edit</button>
     </form>
+
 @endsection
 </body>
 </html>
