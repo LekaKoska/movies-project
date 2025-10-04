3df60b84bc64bfd432a9a5bc4db8a4b7e33f4e74
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-04 13:42:43 +0200

    * Grouping functions by controller and middleware.

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,16 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecommendedController;
 use App\Http\Controllers\UserMoviesController;
 use App\Http\Middleware\AdminCheckMiddleware;
+use App\Models\MoviesModel;
 use App\Models\UserMoviesModel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/',  'welcome')
 ->name("homepage");
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -25,18 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
-
-Route::get("/movies", [MoviesController::class, "allMovies"])
-->name("movies.all");
-Route::get("/search", [MoviesController::class, "search"])
-    ->name("movies.search");
-
-Route::get("movies/{movie:title}", [MoviesController::class, "permalink"])
-    ->name("movies.permalink");
-Route::post("/add/new", [MoviesController::class, "add"])
-    ->name("movies.add");
 
 Route::get("/movies/favourites", function ()
 {
@@ -53,48 +41,45 @@ Route::get("/movies/favourites", function ()
         )->get();
     }
     return view("movies.favourites", compact("userFavourites"));
-})
-->name("movies.favourites");
+})->name("movies.favourites");
 
-Route::view("/genres", "movies.allGenres")
-->name("movies.genre");
-
-Route::get("/genres/results/{genre:genre}", [GenreController::class, "genres"])
-    ->name("movies.genreResults");
-
-
-
-Route::post("movies/comment/{movie}", [CommentController::class, "comment"])
-->name("movies.comment");
-
-Route::view("/movies/add", 'movies.addMovies')
-->name("movies.addForm");
-
-
-
-Route::get("/movies/addFavourite/{movie}", [UserMoviesController::class, "favourite"])
-->name("movies.favourite");
-
-Route::get("/movies/unfavourite/{movie}", [UserMoviesController::class, "unfavourite"])
-    ->name("movies.unfavourite");
-
-
-Route::middleware(['auth', AdminCheckMiddleware::class])->prefix("admin")->group(function ()
+Route::controller(MoviesController::class)->middleware('auth')->group(function ()
 {
-    Route::controller(AdminMoviesController::class)->prefix("/movies")->group(function ()
-    {
-        Route::get("/all", "allMovies");
-        Route::get("/delete/{id}",  "delete")
-            ->name("movie.delete");
-        Route::get("/edit/{movie}",  "edit")
-        ->name("movie.edit");
-        Route::get("/save/{saveMovie}",  "save")
-            ->name("movie.edit");
-        Route::get("/all-users", "users");
+    Route::get("/movies",  "allMovies")->name("movies.all");
+    Route::get("/search",  "search")->name("movies.search");
+    Route::get("movies/{movie:title}",  "permalink")->name("movies.permalink");
+});
 
-    });
+Route::view("/genres", "movies.allGenres")->name("movies.genre");
+
+Route::get("/genres/results/{genre:genre}", [GenreController::class, "genres"])->name("movies.genreResults");
+
+
+Route::controller(CommentController::class)->prefix('movies/comment')->name('movies.')->middleware('auth')->group(function ()
+{
+    Route::post("{movie}",  "comment")->name("comment");
+    Route::get("{comment}/delete", "delete")->name("comment.delete");
 
 });
+
+Route::controller(UserMoviesController::class)->prefix('movies')->name('movies.')->middleware('auth')->group(function ()
+{
+    Route::get("/addFavourite/{movie}",  "favourite")->name("favourite");
+    Route::get("/unfavourite/{movie}",  "unfavourite")->name("unfavourite");
+});
+
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix("admin")->group(function () {
+    Route::controller(AdminMoviesController::class)->prefix("/movies")->group(function () {
+        Route::get("/all", "allMovies")->name('movie.all');
+        Route::get("/delete/{id}",  "delete")->name("movie.delete");
+        Route::get("/edit/{movie}",  "edit")->name("movie.edit");
+        Route::post("/edit/{movie}", "save")->name("movie.save");
+        Route::get("/all-users", "users");
+        Route::view("/add", 'movies.addMovies')->name("movies.addForm");
+        Route::post("/add/new", [MoviesController::class, "add"])->name("movies.add");
+    });
+});
+
 
 
 require __DIR__.'/auth.php';
